5507cc7ad0e23923bdd9347f7ed6c3254ec7f56b
Steve White <stepwhite@gmail.com>
2012-07-09 21:29:52 -0700

    the view engine may have disappeared as we were rendering, so check again before unlocking it (as the pthread lock on null....)

--- OSX/slBreveGLView.mm
+++ OSX/slBreveGLView.mm
@@ -210,7 +210,7 @@ enum {
     brEngineLock(viewEngine);
     camera->renderScene( world, drawCrosshair );
     if(theMovie) [theMovie addFrameFromRGBPixels: [self updateRGBPixels]];
-    brEngineUnlock(viewEngine);
+    if (viewEngine)brEngineUnlock(viewEngine);
 	} else {
 		glClearColor(0.0, 0.0, 0.0, 1.0);
 		glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT|GL_STENCIL_BUFFER_BIT);
